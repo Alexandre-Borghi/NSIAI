6a6180a8141067413cf07a151f4aa3ac336fe875
EwanGL <ewan.grignoux56@gmail.com>
2020-06-17 09:50:23 +0200

    Transphormation of Breakout to Ping Pong, move of ball and score

--- game.py
+++ game.py
@@ -2,10 +2,45 @@
 import sys
 import os
 import pygame
-
+import random
+import time
 # Our Function
 def percentage(percent, whole):
-  return int((percent * whole) / 100.0)
+    return int((percent * whole) / 100.0)
+
+class Player():
+    def __init__(self):
+        self.score = 0
+
+class Plateform(pygame.sprite.Sprite):
+    #This class define platforms
+    
+    def __init__(self):
+        self.speed = percentage(2,y_GameSurface)
+        self.image = pygame.image.load(os.path.join(current_path, 'plateform.png'))
+        self.rect = self.image.get_rect()
+        self.rect.x = percentage(90, x_GameSurface)
+        self.rect.y = percentage(38, y_GameSurface)
+    
+    def move_up(self):
+        if self.rect.y + self.speed > percentage(2.7, y_GameSurface):
+            self.rect.y -= self.speed
+            #print(f"Y: {plateform.rect.y}")
+    def move_down(self):
+        if self.rect.y - self.speed < percentage(68, y_GameSurface):
+            self.rect.y += self.speed
+            #print(f"Y: {plateform.rect.y}")
+
+class Ball(pygame.sprite.Sprite):
+    # This class define the ball
+    
+    def __init__(self):
+        self.image = pygame.image.load(os.path.join(current_path, 'ball.png'))
+        self.rect = self.image.get_rect()
+        self.rect.y = percentage(47, y_GameSurface)
+        self.rect.x = percentage(random.choice([14,86]), x_GameSurface)
+        self.speed_x = percentage(0.2, x_GameSurface)
+        self.speed_y = percentage(0.4, y_GameSurface)
 
 # Our Variables
 x_screen = 1080
@@ -16,83 +51,85 @@ current_path = os.path.dirname(__file__)
 number_bricks = 135
 brick_rect_x = 0
 brick_rect_y = 0
-
-#Our Class
-class Player(pygame.sprite.Sprite):
-    #This class define the current player
-
-    def __init__(self):
-        self.life = 5
-        self.score = 0
-
-class Plateform(pygame.sprite.Sprite):
-    #This class define the platform
-    
-    def __init__(self):
-        self.speed = percentage(5,x_GameSurface)
-        self.image = pygame.image.load(os.path.join(current_path, 'plateform.png'))
-        self.rect = self.image.get_rect()
-        self.rect.x = percentage(38, x_GameSurface)
-        self.rect.y = percentage(85,y_GameSurface)
-
-class Ball(pygame.sprite.Sprite):
-    # This class define the ball
-
-    def __init__(self):
-        self.image = pygame.image.load(os.path.join(current_path, 'ball.png'))
-        self.rect = self.image.get_rect()
-        self.rect.x = percentage(47, x_GameSurface)
-        self.rect.y = percentage(80, y_GameSurface)
-
-class Brick(pygame.sprite.Sprite):
-    #This class define bricks
-
-    def __init__(self):
-        self.image = pygame.image.load(os.path.join(current_path, 'brick.png'))
-        self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 0
+keys_pressed = {}
 
 #Our items
-player = Player()
 plateform = Plateform()
+plateform2 = Plateform()
+plateform2.rect.x = percentage(10,x_GameSurface)
+plateforms = [plateform, plateform2]
 ball = Ball()
-list_bricks = []
-
-for i in range(number_bricks):
-    i = Brick()
-    i.rect.x = brick_rect_x
-    i.rect.y = brick_rect_y
-    brick_rect_x +=10
-    if (brick_rect_x + 100) > x_GameSurface:
-        brick_rect_y += 30
-        brick_rect_x = 0
-    list_bricks.append(i)
+player1 = Player()
+player2 = Player()
 
 #Graphic interface
 pygame.display.init()
-pygame.display.set_caption("Breakout AI")
+pygame.display.set_caption("Ping Pong AI")
 Window = pygame.display.set_mode((x_screen,y_screen))
 
 #Our Surface
 GameSurface = pygame.Surface((x_GameSurface, y_GameSurface))
-GameSurface.fill((255,255,255))
-
 
 #Game loop
 running = True
 while running :
-    pygame.display.update()
-    Window.blit(GameSurface, (0,0))
-    GameSurface.blit(plateform.image, plateform.rect)
-    GameSurface.blit(ball.image, ball.rect)
     
-    for items in list_bricks:
-        GameSurface.blit(items.image, items.rect)
+    pygame.display.update()
+    GameSurface.fill((255,255,255))
 
+    #Blit items and surface
+    GameSurface.blit(plateform.image, plateform.rect)
+    GameSurface.blit(plateform2.image, plateform2.rect)
+    GameSurface.blit(ball.image, ball.rect)
+    Window.blit(GameSurface, (0,0))
+
+    #Ball move
+    if ball.rect.x > plateform2.rect.x and ball.rect.x < plateform.rect.x:
+        if len(pygame.sprite.spritecollide(ball, plateforms, False, pygame.sprite.collide_mask)) == 0:
+            ball.rect.x += ball.speed_x
+            ball.rect.y += ball.speed_y
+            #print(f'Speed_Y: {ball.speed_y}   |    Y: {ball.rect.y}')
+            #print('normal')
+        else:
+            ball.speed_x *= -1
+            ball.rect.x += ball.speed_x
+            ball.rect.y += ball.speed_y
+            #print(f'collision: speed_y= {ball.speed_y} ')
+
+        if ball.rect.y < percentage(2.7, y_GameSurface) or ball.rect.y > percentage(68, y_GameSurface):
+            ball.speed_y *= -1
+    
+    elif ball.rect.x <= plateform2.rect.x:
+        player2.score += 1
+        print(f'Les scores sont: {player1.score} point(s) pour le joueur de gauche et {player2.score} pour le joueur de droite')
+        ball.rect.y = percentage(47, y_GameSurface)
+        ball.rect.x = percentage(random.choice([14,86]), x_GameSurface)
+        pygame.display.update()
+        time.sleep(1)
+        #print (f'X: {ball.rect.x}   |   Y: {ball.rect.y}')
+
+    elif ball.rect.x >= plateform.rect.x:
+        player1.score += 1
+        print(f'Les scores sont: {player1.score} point(s) pour le joueur de gauche et {player2.score} pour le joueur de droite')
+        ball.rect.y = percentage(47, y_GameSurface)
+        ball.rect.x = percentage(random.choice([14,86]), x_GameSurface)
+        pygame.display.update()
+        time.sleep(1)
+        #print (f'X: {ball.rect.x}   |   Y: {ball.rect.y}')
+
+    #Events loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                plateform.move_up()
+            elif event.key == pygame.K_DOWN:
+                plateform.move_down()
+            elif event.key == pygame.K_z:
+                plateform2.move_up()
+            elif event.key == pygame.K_s:
+                plateform2.move_down()
 
 sys.exit()
